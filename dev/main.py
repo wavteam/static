a114ef330458a44f836851a024a2c61836701ad0
Dmitry Kotov <dmitrijkotov634@gmail.com>
2024-04-01 22:12:41 +0300

    Added dev

--- dev/main.py
+++ dev/main.py
@@ -1,12 +1,11 @@
 import json
+import jsoneditor
 import logging
 import re
-from dataclasses import dataclass
-from pprint import pprint
-
-import jsoneditor
 import requests
+from dataclasses import dataclass
 from fake_useragent import UserAgent
+from pprint import pprint
 
 ua = UserAgent()
 
@@ -55,63 +54,72 @@ def format_by_pattern(input_string, phone):
     return new_string.replace("{full_phone}", str(phone)).replace("{phone}", phone.phone)
 
 
+def process_request(request):
+    url = format_by_pattern(request["url"], phone)
+    logging.info("URL: %s", url)
+
+    params = {
+        "method": request["method"],
+        "url": url,
+        "headers": {"User-Agent": ua.random}
+    }
+
+    logging.info("Method: %s", request["method"].upper())
+
+    if "headers" in request:
+        for k, v in request["headers"].items():
+            formatted_key = format_by_pattern(k, phone)
+            formatted_value = format_by_pattern(v, phone)
+            params["headers"][formatted_key] = formatted_value
+
+        logging.info("Headers: %s", params["headers"])
+
+    if "json" in request:
+        json_body = format_by_pattern(json.dumps(request["json"]), phone)
+
+        try:
+            json.loads(json_body)
+        except Exception as e:
+            logging.warning("INVALID JSON BODY: %s, Error: %s", json_body, str(e))
+
+        logging.debug("JSON Body: %s", json_body)
+
+        params["json"] = json_body
+
+    if "data" in request:
+        formdata = {
+            format_by_pattern(k, phone): format_by_pattern(v, phone)
+            for k, v in request["data"].items()
+        }
+
+        logging.debug("Form data Body: %s", formdata)
+
+        params["data"] = formdata
+
+    logging.debug("Sending request with params: %s", params)
+
+    try:
+        response = requests.request(**params)
+        try:
+            pprint(response.json())
+        except json.JSONDecodeError:
+            print(response.text)
+    except requests.RequestException as e:
+        logging.error("Request failed: %s", str(e))
+
+
 def on_result(result):
     print(json.dumps(result))
 
-    for index, request in enumerate(result["requests"]):
-        logging.info("Request #%s", index)
+    with open("service.json", "w", encoding="UTF-8") as file:
+        file.write(json.dumps(result))
 
-        url = format_by_pattern(request["url"], phone)
-        logging.info("URL: %s", url)
-
-        params = {
-            "method": request["method"],
-            "url": url,
-            "headers": {"User-Agent": ua.random}
-        }
-
-        logging.info("Method: %s", request["method"].upper())
-
-        if "headers" in request:
-            for k, v in request["headers"].items():
-                formatted_key = format_by_pattern(k, phone)
-                formatted_value = format_by_pattern(v, phone)
-                params["headers"][formatted_key] = formatted_value
-
-            logging.info("Headers: %s", params["headers"])
-
-        if "json" in request:
-            json_body = format_by_pattern(json.dumps(request["json"]), phone)
-
-            try:
-                json.loads(json_body)
-            except Exception as e:
-                logging.warning("INVALID JSON BODY: %s, Error: %s", json_body, str(e))
-
-            logging.debug("JSON Body: %s", json_body)
-
-            params["json"] = json_body
-
-        if "data" in request:
-            formdata = {
-                format_by_pattern(k, phone): format_by_pattern(v, phone)
-                for k, v in request["data"].items()
-            }
-
-            logging.debug("Form data Body: %s", formdata)
-
-            params["data"] = formdata
-
-        logging.debug("Sending request with params: %s", params)
-
-        try:
-            response = requests.request(**params)
-            try:
-                pprint(response.json())
-            except json.JSONDecodeError:
-                print(response.text)
-        except requests.RequestException as e:
-            logging.error("Request failed: %s", str(e))
+    if "requests" in result:
+        for index, request in enumerate(result["requests"]):
+            logging.info("Request #%s", index)
+            process_request(request)
+    else:
+        process_request(result)
 
 
 @dataclass
